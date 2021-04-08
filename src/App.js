7763c368b375edf1c6385b690e735b8f6f0709e6
prahal <vprahaladhan@gmail.com>
2021-04-08 11:55:43 +1000

    Done.

--- src/App.js
+++ src/App.js
@@ -3,29 +3,19 @@ import React, { Component } from 'react';
 import Timer from './Timer'
 
 class App extends Component {
-
   //no props being used here, so we can use the shorthand declaration of state
   state = {
     timerIDs: []
   }
-
 
   //Your code here:
   componentDidMount() {
     this.handleAddTimer();
   };
 
-
-
-
-
-
-
-
   // No need to modify anything in render or the class methods below
   // Unless, of course, you're curious about how it all works
   render() {
-
     return (
       <div className="App">
         <h1>MultiTimer</h1>
@@ -56,9 +46,7 @@ class App extends Component {
     this.setState(prevState => ({
       timerIDs: prevState.timerIDs.filter(timer_id => timer_id !== id)
     }))
-  }
-
-
+  };
 }
 
 export default App;
